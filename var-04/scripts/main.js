04fd09ca8c028b2c1280ff71965a26ac7fb33271
loudovsky <luileptitecossais@gmail.com>
2024-01-21 19:16:57 +0100

    evening update

--- var-04/scripts/main.js
+++ var-04/scripts/main.js
@@ -1,10 +1,28 @@
 let myTab = [4, 60, 88, 127, 36, 95, 44, 55];
 let text = "";
+let myButton = document.querySelector('.button');
 
 for (let i = 0; i < myTab.length; i++) {
         text += "<li>" + myTab[i] + "</li>"
 }
 
 document.getElementsByTagName("ul")[0].innerHTML = text;
+
+function numAverage() { 
+        let sum = 0;
+        for (let i = 0; i < myTab.length; i++) {
+        sum += myTab[i]; 
+        }
+        return sum / myTab.length;
+}
+
+
+
+myButton.addEventListener('click',function () {
+        let moyenne = numAverage(myTab);
+        document.getElementsByTagName("p")[0].innerHTML = moyenne;
+
+});
+
 
 
